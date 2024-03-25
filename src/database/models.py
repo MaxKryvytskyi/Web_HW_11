d5ba2a1db88837031e6c6cec52120000d9e585e8
Max <maxkryvytskyiwork@gmail.com>
2024-03-25 03:30:29 +0200

    save

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.sql.sqltypes import Date
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
